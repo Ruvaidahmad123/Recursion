72364637862c1be1aeb3450ebb18682ade563d8c
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-05-25 21:16:03 +0530

    Add files via upload

---
+++ ReverseusingRecursion.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class ReverseusingRecursion
+{ 
+    public static int[] reverse(int[] arr,int l, int r){
+        if(l>r){   //also u can do till l<=n/2
+            return arr;
+        }
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        reverse(arr,l+1,r-1);
+        return arr;
+    }
+	public static void main(String[] args) {
+	    int arr[]={2,3,5,7};
+	    int l=0;
+	    int r=arr.length-1;
+		int rev[]=reverse(arr,l,r);
+		for(int x:rev){
+		   System.out.print(x+" ");
+		}
+	}
+}
